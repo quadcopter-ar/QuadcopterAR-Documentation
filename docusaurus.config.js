833f51d5bc464d4a05f4c84438de1a17938fd78b
Vybhav Achar Bhargav <vybhavab@gmail.com>
2022-02-22 00:36:43 -0800

    fix(): fixing broken links

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -87,10 +87,10 @@ const config = {
           {
             title: 'More',
             items: [
-              {
-                label: 'Blog',
-                to: '/blog',
-              },
+              // {
+              //   label: 'Blog',
+              //   to: '/blog',
+              // },
               {
                 label: 'GitHub',
                 href: 'https://github.com/quadcopter-ar/',
